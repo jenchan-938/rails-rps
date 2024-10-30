f66d2941e51f20806f8b2e54c28da869b83e9c46
jenchan-938 <jchan5@uchicago.edu>
2024-10-30 02:33:16 +0000

    9:33

--- app/controllers/rock_c.rb
+++ app/controllers/rock_c.rb
@@ -1,5 +1,20 @@
 class rock_c < ApplicatonController
   def play 
-    render ( {:plain => "howdy"})
-  end
+    @options = ["rock", "paper", "scissors"]
+    @robot = @options.sample
+    @robot_play = "They played #{@robot}"
+      
+    @user_play = "We played rock"
+
+    if @robot == "rock"
+      @result = "We tied!"
+    elsif @robot =="paper"
+      @result = "We lost!"
+    elsif @robot =="scissors"
+        @result = "We won!"
+    end
+ return @result
+ render ({ :template => "/rock.html.erb"})
+end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/rock", { :controller => "rock_c", :action => "play"})
+
+  get("/scissor", { :controller => "scissor_c", :action => "play"})
+
+  get("/paper", { :controller => "paper_c", :action => "play"})
 end
