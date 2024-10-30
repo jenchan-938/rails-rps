9d77585ce3a7aca9d0b5ae9f316e19146db836fa
jenchan-938 <jchan5@uchicago.edu>
2024-10-30 02:19:06 +0000

    9:19

---
+++ app/controllers/rock_c.rb
@@ -0,0 +1,5 @@
+class rock_c < ApplicatonController
+  def play 
+    render ( {:plain => "howdy"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-
+  get("/rock", { :controller => "rock_c", :action => "play"})
 end
